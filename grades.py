65b648a7085b6b9bc19cbef129583885d99b83b5
Neil Narnaware <neilnarnaware123@gmail.com>
2021-02-09 14:21:36 -0600

    Update grades.py

--- grades.py
+++ grades.py
@@ -3,7 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 
-students = ["sbellu3@uic.edu","cchalk4@uic.edu","sfatim36@uic.edu","nhamda2@uic.edu","yhasna2@uic.edu","biden2@uic.edu","nirfan2@uic.edu","nlang6@uic.edu","elee232@uic.edu","slee664@uic.edu","ogonza21@uic.edu","tlung2@uic.edu","tmaato2@uic.edu","ynaray3@uic.edu","rodonn3@uic.edu","koye2@uic.edu","tqadri2@uic.edu","osalce3@uic.edu","ssheik30@uic.edu","ksusza2@uic.edu","ktu4@uic.edu","ruribe2@uic.edu","dvera9@uic.edu","rzadda2@uic.edu"]
+students = []
 driver_path = "E:\Setups\Selenium Chrome 85\chromedriver.exe"
 driver = webdriver.Chrome(driver_path)
 driver.maximize_window()
@@ -14,11 +14,11 @@ driver.refresh()
 driver.get("https://www.gradescope.com/courses/81148")
 time.sleep(10)
 element_send = driver.find_element_by_id("session_email")
-element_send.send_keys("nnarna2@uic.edu")
+element_send.send_keys("")
 time.sleep(10)
 
 element_send = driver.find_element_by_id("session_password")
-element_send.send_keys("Sribalaji@02")
+element_send.send_keys("")
 time.sleep(10)
 
 driver.find_element_by_xpath("/html/body/div[1]/div[2]/form/div[4]/input").click()
